3b552362378cee0ca798a2f87fdc75a4fa7fdb08
bliferniz <vitalij.bliferniz@web.de>
2017-06-13 20:36:01 +0200

    fix

--- Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,16 @@
 require 'httparty'
+require 'pp'
 
 class Recipe
+	include HTTParty
 
+	base_uri 'http://food2fork.com/api'
+	default_params({key: ENV["FOOD2FORK_KEY"]})
+	format :json
+
+	def self.for(search_term = '')
+		get('/search', query: { q: search_term})['recipes']
+	end
 end
+
+pp Recipe.for 'Chocolate'
